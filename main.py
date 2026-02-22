a231e18457ca4463c49bb9d43262ea2be6e108c3
TCañete-10R <canete.tc@obmontessori.edu.ph>
2026-02-22 17:43:40 +0800

    Refactor eligibility check and add account creation

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
 from pyscript import display, document, HTML
 
+def show_signup(e):
+    document.getElementById("signup_section").style.display = "block"
+    document.getElementById("checker_section").style.display = "none"
+    document.getElementById("players_section").style.display = "none"
+
+def show_checker(e):
+    document.getElementById("signup_section").style.display = "none"
+    document.getElementById("checker_section").style.display = "block"
+    document.getElementById("players_section").style.display = "none"
+
+def show_players(e):
+    document.getElementById("signup_section").style.display = "none"
+    document.getElementById("checker_section").style.display = "none"
+    document.getElementById("players_section").style.display = "block"
+
+def create_account(e):
+    username = document.getElementById("new_username").value
+    password = document.getElementById("new_password").value
+
+    if not username or not password:
+        display("Please fill in all fields.", target="signup_output", append=False)
+    else:
+        display("Account created. You may now log in using your credentials.",
+                target="signup_output", append=False)
+
 def check_eligibility(e):
-    reg_element = document.querySelector('input[name="registration"]:checked')
-    med_element = document.querySelector('input[name="medical"]:checked')
-    
+    reg = document.querySelector('input[name="registration"]:checked')
+    med = document.querySelector('input[name="medical"]:checked')
     grade = document.getElementById("grade_level").value
     section = document.getElementById("section").value
-    
-    if not reg_element or not med_element or not grade or not section:
-        display("𝒫𝓁𝑒𝒶𝓈𝑒 𝒸𝑜𝓂𝓅𝓁𝑒𝓉𝑒 𝒶𝓁𝓁 𝒻𝒾𝑒𝓁𝒹𝓈!", target="output", append=False)
-        document.getElementById("image_container").innerHTML = ""
+
+    if not reg or not med or not grade or not section:
+        display("Please complete all fields!", target='output', append=False)
         return
 
-    registration = reg_element.value
-    medical = med_element.value
-    grade_num = int(grade)
-
-    # Registration logic
-    if registration == "no":
-        display("𝒫𝓁𝑒𝒶𝓈𝑒 𝓇𝑒𝑔𝒾𝓈𝓉𝑒𝓇 𝑜𝓃𝓁𝒾𝓃𝑒 𝒻𝒾𝓇𝓈𝓉 𝓉𝑜 𝓀𝓃𝑜𝓌 𝓎𝑜𝓊𝓇 𝓉𝑒𝒶𝓂.", target="output", append=False)
-        document.getElementById("image_container").innerHTML = ""
-        return
-        
-    # Medical clearance logic
-    elif medical == "no":
-        display("𝒫𝓁𝑒𝒶𝓈𝑒 𝓈𝑒𝒸𝓊𝓇𝑒 𝒶 𝓂𝑒𝒹𝒾𝒸𝒶𝓁 𝒸𝓁𝑒𝒶𝓇𝒶𝓃𝒸𝑒 𝒻𝓇𝑜𝓂 𝓉𝒽𝑒 𝒸𝓁𝒾𝓃𝒾𝒸.", target="output", append=False)
-        document.getElementById("image_container").innerHTML = ""
+    if reg.value == "no":
+        display("Please register online first.", target='output', append=False)
         return
 
-    # Eligibility and Team Assignment logic
-    if 7 <= grade_num <= 10:
-        team = ""
-        img_url = ""
-        
-        # Mapping Grade Levels to Team Images via HTTPS
-        if grade_num == 7:
-            team = "Green Hornets"
-            img_url = "https://image2url.com/r2/default/images/1769128301686-963161df-7925-401c-b8bb-fd19d9ea6d05.jpg"
-        elif grade_num == 8:
-            team = "Red Bulldogs"
-            img_url = "https://image2url.com/r2/default/images/1769128261350-fb0d516a-14b2-4336-b729-70a56e7d72b4.jpg"
-        elif grade_num == 9:
-            team = "Yellow Tigers"
-            img_url = "https://image2url.com/r2/default/images/1769128065235-3eff06e9-f4d8-47ec-9cd3-f4c2f38afac6.jpg"
-        elif grade_num == 10:
-            team = "Blue Bears"
-            img_url = "https://image2url.com/r2/default/images/1769128322171-8184258e-ff9e-498a-ae1b-d97225bc6c9d.jpg"
+    if med.value == "no":
+        display("Please secure a medical clearance.", target='output', append=False)
+        return
 
-        message = f"Congratulations!<br>You are part of the <b>{team}</b>."
-        display(HTML(message), target="output", append=False)
-        
-        img_html = f"<img src='{img_url}' height='300' width='350' style='border: 4px solid #800000;' alt='{team}'>"
-        document.getElementById("image_container").innerHTML = img_html
-        
+    grade = int(grade)
+
+    if 7 <= grade <= 10:
+        teams = {
+            7: "Green Hornets",
+            8: "Red Bulldogs",
+            9: "Yellow Tigers",
+            10: "Blue Bears"
+        }
+        display(f"Congratulations! You are part of the {teams[grade]}.",
+                target='output', append=False)
     else:
-        display("Sorry, Intramurals are only for Grades 7 to 10.", target="output", append=False)
-        document.getElementById("image_container").innerHTML = ""
+        display("Intramurals are only for Grades 7-10.",
+                target='output', append=False)
